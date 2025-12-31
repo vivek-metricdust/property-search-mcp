0202218f0978ba0533bf1d4253e2a42674fde73a
vivek-metricdust <vivek.v@metricdust.com>
2025-12-31 17:45:04 +0530

    reverted

--- serve_http.bat
+++ serve_http.bat
@@ -1,3 +1,3 @@
 @echo off
-python property-search-mcp.py --transport sse --port 8000 --path /property-search > server_v3.log 2>&1
+python property-search-mcp.py --transport sse --port 8000 --path /sse
 pause
